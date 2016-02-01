ca5578607206d30274284641a60931b6e1984242
Bert <Bert>
2016-02-01 12:43:42 -0700

    buttons labeled

---
+++ app/app.js
@@ -0,0 +1,13 @@
+app.controller("CalculatorController", function(){
+    
+  app.controller("ViewController", function(){
+      
+      this.viewSetter = function(clicked){
+      this.currentView=clicked;
+      }
+  })  
+    
+    
+    
+    
+})
